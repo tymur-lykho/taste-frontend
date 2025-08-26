06d3044e77b884fe649530820c7be6e452418ff6
Tymur Lykho <tima.likho@gmail.com>
2025-08-26 17:47:04 +0300

    Fixed container component

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,13 +2,16 @@
 import Navigation from "../Navigation/Navigation";
 import css from "./AppBar.module.css";
 import MainLogo from "../MainLogo/MainLogo.jsx";
+// import Container from "../../reuseable/Container/Container.jsx";
 // import UserMenu from "../UserMenu/UserMenu";
 
 export default function AppBar() {
   return (
     <header className={css.header}>
+      {/* <Container> */}
       <MainLogo />
       <Navigation />
+      {/* </Container> */}
     </header>
   );
-};
+}
