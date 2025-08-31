3e56b0a29ec8c1199d321a2e39f6b03d8e1a853f
Alex Khavin <alexandr_havin@yahoo.com>
2025-08-31 22:00:21 +0300

    add recipes to favorite and delete

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -5,25 +5,47 @@ import Time from "../Time/Time";
 import css from "./RecipeCard.module.css";
 import Icon from "../../reuseable/Icon/Icon";
 import { useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import { addFavoritesRecipe, removeFromFavorites } from "../../redux/recipes/operations";
+import { selectFavoriteRecipes } from "../../redux/recipes/selectors";
 
 export default function RecipesCard({ recipe }) {
   const dispatch = useDispatch();
   const location = useLocation();
+  const favoriteRecipes = useSelector(selectFavoriteRecipes);
+
+  const [isFavorite, setIsFavorite] = useState(recipe.isFavorite || false);
 
   const isOwnRecipesRoute = location.pathname.includes('/profile/own');
   // const isFavoritesRecipesRoute = location.pathname.includes('/profile/favorites');
 
-  const handleAddToFavorites = (recipeId) => {
-    console.log("Adding recipe ID:", recipeId); ///
-    dispatch(addFavoritesRecipe(recipeId));
+  useEffect(() => {
+    const favorite = favoriteRecipes.some(fav => fav._id === recipe._id);
+    setIsFavorite(favorite);
+  }, [recipe._id, favoriteRecipes]);
+
+  const handleToggleFavorite = () => {
+    if (isFavorite) {
+      console.log("Removing from favorites:", recipe._id);
+      dispatch(removeFromFavorites(recipe._id));
+      setIsFavorite(false);
+    } else {
+      console.log("Adding to favorites:", recipe._id);
+      dispatch(addFavoritesRecipe(recipe._id));
+      setIsFavorite(true);
+    }
   };
 
-  const handleRemoveFromFavorites = (recipeId) => {
-    console.log("Removing recipe ID:", recipeId); ///
-    dispatch(removeFromFavorites(recipeId));
-  };
+  // const handleAddToFavorites = (recipeId) => {
+  //   console.log("Adding recipe ID:", recipeId);
+  //   dispatch(addFavoritesRecipe(recipeId));
+  // };
+
+  // const handleRemoveFromFavorites = (recipeId) => {
+  //   console.log("Removing recipe ID:", recipeId);
+  //   dispatch(removeFromFavorites(recipeId));
+  // };
 
   return (
     <div className={css["card"]}>
@@ -53,6 +75,24 @@ export default function RecipesCard({ recipe }) {
 
         {!isOwnRecipesRoute && (
           <Button 
+            className={clsx("white", {
+              [css.favoriteActive]: isFavorite
+            })} 
+            title={isFavorite ? "Remove from favorite" : "Add to favorite"} 
+            aria-label={isFavorite ? "Remove from favorite" : "Add to favorite"}
+            onClick={handleToggleFavorite}
+          >
+            <Icon 
+              className={clsx(css["save-icon"], {
+                [css.active]: isFavorite
+              })} 
+              iconName="save-icon" 
+            />
+          </Button>
+        )}
+
+        {/* {!isOwnRecipesRoute && (
+          <Button 
             className="white" 
             title="Add to favorite" 
             aria-label="Add to favorite"
@@ -68,7 +108,7 @@ export default function RecipesCard({ recipe }) {
               iconName="save-icon" 
             />
           </Button>
-        )}
+        )} */}
 
 
         {/* {!isOwnRecipesRoute && (
